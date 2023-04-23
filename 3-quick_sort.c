18ac1b0b12ba3ec1abfd222efaaf983b4fbbf531
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-04-23 20:46:53 +0300

    update task3

---
+++ 3-quick_sort.c
@@ -0,0 +1,49 @@
+#include "sort.h"
+
+void quick_sort(int *array, size_t size)
+{
+	quicksort_helper(0, size - 1, array, size);
+}
+/**
+ * quicksort_helper - auxiliar function for the
+ * quick_sort function
+ * @a: input arrray
+ * @low: first element
+ * @high: last element
+ * @size: size of the array
+ * Return: no return
+ */
+void quicksort_helper(int below, int above, int *a, int size)
+{
+	int top, btm, i;
+	int tmp;
+
+	if (below < above)
+	{
+		top = above;
+		btm = below;
+		for (i = below; i < above; i++)
+		{
+			if (a[i] < a[top])
+			{
+				if (i != btm)
+				{
+					tmp = a[i];
+					a[i] = a[btm];
+					a[btm] = tmp;
+					print_array(a, size);
+				}
+				btm++;
+			}
+		}
+		if (btm != top && a[btm] != a[top])
+		{
+			tmp = a[btm];
+			a[btm] = a[top];
+			a[top] = tmp;
+			print_array(a, size);
+		}
+		quicksort_helper(below, btm - 1, a, size);
+		quicksort_helper(btm + 1, above, a, size);
+	}
+}
