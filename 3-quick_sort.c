0100bb73a6531796ca817a3424a9b6286d26f297
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-04-23 21:56:41 +0300

    Update 3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,5 +1,17 @@
 #include "sort.h"
 
+/**
+ * quick_sort - function that sorts an array
+ * of integers in ascending order
+ *
+ * @array: input arrray
+ * @size: size of the array
+ * Return: no return
+ */
+void quick_sort(int *array, size_t size)
+{
+	quicksort_helper(0, size - 1, array, size);
+}
 
 /**
  * quicksort_helper - auxiliar function for the
@@ -10,10 +22,6 @@
  * @size: size of the array
  * Return: no return
  */
-void quick_sort(int *array, size_t size)
-{
-	quicksort_helper(0, size - 1, array, size);
-}
 
 void quicksort_helper(int below, int above, int *a, int size)
 {
